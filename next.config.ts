83dcbf14271a0ca14bee6d29512b424d2280d976
Igor Peshkov <igapekaandr@gmail.com>
2025-01-22 02:26:28 +0200

    k

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // output: 'export',
-  // basePath: '/hrm-concept',
+  output: 'export',
+  basePath: '/hrm-concept',
 };
 
 export default nextConfig;
