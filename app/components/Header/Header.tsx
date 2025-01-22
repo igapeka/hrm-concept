41a71cbc76bdcfc2664198022057e46f0cb29614
Igor Peshkov <igapekaandr@gmail.com>
2025-01-22 02:18:39 +0200

    d

--- app/components/Header/Header.tsx
+++ app/components/Header/Header.tsx
@@ -11,7 +11,7 @@ import { applist } from "@/app/applist";
 export default function Header() {
   const pathname = usePathname().split("/")[1]; // "/dom/foo" -> "dom"
   const currentApp = applist.filter((app) => app.id.includes(pathname))[0];
-  const currentAppName = currentApp.name;
+  const currentAppName = currentApp ? currentApp.name : "";
   return (
     <div className={header.header}>
       <div className={header.logonamewrap}>
