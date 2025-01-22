1ea907bcbc39ef1de1837d27b7a6f06e7035cc2c
Igor Peshkov <igapekaandr@gmail.com>
2025-01-22 02:33:42 +0200

    d

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 import type { NextConfig } from "next";
+const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  basePath: '/hrm-concept',
+  assetPrefix: isProd ? '/hrm-concept/' : '',
+  basePath: isProd ? '/hrm-concept' : '',
 };
 
 export default nextConfig;
